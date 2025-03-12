984853e788b3a12b52c9256d67c8adb029e0293e
Herlan <cahuayakory777@gmail.com>
2025-03-12 12:01:11 -0400

    agrego de codigo python (desarrollador2)

---
+++ operacion.py
@@ -0,0 +1,5 @@
+#operacion simple
+numero1 = int(input("ponga un numero: "))
+numero2 = int(input("ponga un numero: "))
+suma = numero1 + numero2
+print("la suma de", numero1,"y de",numero2,"es",suma)
